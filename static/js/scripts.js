3ea36d1623089e9619feee0c08dc6e79cc3ab5d4
fgsamuel <fg.samuel@gmail.com>
2015-09-02 17:28:20 -0300

    Troca datepicker jquery pelo datepicker bootstrap

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,25 +1,13 @@
 $(function() {
 	carregaDatepicker();
-	carregaTab();
   carregaSelect2();
 });
 
 function carregaDatepicker(){
-    //cria o datepicker com op��o de mes e ano
 	$( ".datepicker" ).datepicker({
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "c-100:c"
-      }, $.datepicker.regional[ "pt-BR" ]);
-
-	//retira a edicao do campo
-	$('.datepicker').attr('editable', false);
+        format: 'dd/mm/yyyy',
+      });
   }
-
-function carregaTab(){
-	$( ".tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
-    $( ".tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
-}
 
 //Configura o componente chosen
 function carregaSelect2(){
